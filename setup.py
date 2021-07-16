9abb7eeb9a74897452504fd3c7af244a992e8590
Abstract-X <abstract-x-mail@protonmail.com>
2021-07-17 01:17:11 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from pathlib import Path
+import re
+
+
+def fetch_version() -> str:
+
+    content = (Path(__file__).parent / "tgbotscenario" / "__init__.py").read_text(encoding="UTF-8")
+    try:
+        version_string = re.findall(r'__version__ = \"\d+\.\d+\.\d+\"', content)[0]
+        version = version_string.rsplit(" ", 1)[-1].replace('"', '')
+    except IndexError:
+        raise RuntimeError("version not found!")
+
+    return version
+
+
+setuptools.setup(
+    name="tgbotscenario",
+    version=fetch_version(),
+    packages=setuptools.find_packages(exclude=("tests",)),
+    url="https://github.com/Abstract-X/tgbotscenario",
+    license="MIT",
+    author="Abstract-X",
+    author_email="abstract-x-mail@protonmail.com",
+    description="Scene-based FSM implementation for Telegram bots.",
+    python_requires='>=3.7'
+)
