c86c050f8b2790d681146ade383339d53d445299
Leo Trubach <leotrubach@gmail.com>
2021-07-25 22:17:14 +0600

    Refactor memory state storage

--- tgbotscenario/synchronous/states/storages/memory.py
+++ tgbotscenario/synchronous/states/storages/memory.py
@@ -1,28 +1,15 @@
+from collections import defaultdict
 from typing import List
 
 from tgbotscenario.synchronous.states.storages.base import AbstractStateStorage
 
 
 class MemoryStateStorage(AbstractStateStorage):
-
     def __init__(self):
-
-        self._storage = {}
+        self._storage = defaultdict(list)
 
     def load(self, *, chat_id: int, user_id: int) -> List[str]:
-
-        try:
-            states = self._storage[chat_id][user_id][:]
-        except KeyError:
-            states = []
-
-        return states
+        return self._storage[chat_id, user_id][:]
 
     def save(self, states: List[str], *, chat_id: int, user_id: int) -> None:
-
-        try:
-            users_ids = self._storage[chat_id]
-        except KeyError:
-            self._storage[chat_id] = users_ids = {}
-
-        users_ids[user_id] = states[:]
+        self._storage[chat_id, user_id] = states[:]
