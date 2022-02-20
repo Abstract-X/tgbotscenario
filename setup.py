7b42dc456168b725680531876b974cb399a3c198
Abstract-X <abstract-x-mail@protonmail.com>
2022-02-20 20:39:35 +0300

    Add «xcept» requirement

--- setup.py
+++ setup.py
@@ -9,14 +9,18 @@ setuptools.setup(
     license="MIT",
     author="Abstract-X",
     author_email="abstract-x-mail@protonmail.com",
-    description="Scene-based FSM implementation for Telegram bots.",
+    description="A scene-based FSM implementation for Telegram bots.",
     classifiers=[
         "Development Status :: 1 - Planning",
+        "Operating System :: OS Independent",
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9"
+    ],
+    install_requires=[
+        "xcept>=2.0.0"
     ],
     python_requires='>=3.7'
 )
